8a70e0acafa7f48669b7d8e33aa0a78b38a60575
Miro Svrtan <miro@mirosvrtan.me>
2015-05-08 00:21:04 +0200

    wip: A1

--- src/NullDev/Nemesis/CodeBuilder/NikicPhpParser/Builder.php
+++ src/NullDev/Nemesis/CodeBuilder/NikicPhpParser/Builder.php
@@ -3,6 +3,7 @@
 namespace NullDev\Nemesis\CodeBuilder\NikicPhpParser;
 
 use PhpParser;
+use PhpParser\Node\Expr\Variable;
 
 class Builder
 {
@@ -13,5 +14,102 @@ class Builder
     public function __construct()
     {
         $this->factory = new PhpParser\BuilderFactory();
+    }
+
+    public function getNode()
+    {
+        return $this->data->getNode();
+    }
+
+    public function createMethod($methodName)
+    {
+        $this->data = $this->factory->method($methodName)->makePublic();
+
+        return $this;
+    }
+
+    public function getMethodParamVariableName($param)
+    {
+        return 'mock'.ucfirst($param->getName());
+    }
+
+    public function mockeryMock($class = false)
+    {
+        if ($class === false) {
+            $classInstanceName = [];
+        } else {
+            $classInstanceName = [
+                new PhpParser\Node\Name(var_export($class, true)),
+            ];
+        }
+
+        return new PhpParser\Node\Expr\StaticCall(
+            new PhpParser\Node\Name('m'),
+            new PhpParser\Node\Name('mock'),
+            $classInstanceName
+        );
+    }
+
+    public function variable($name)
+    {
+        return new PhpParser\Node\Expr\Variable($name);
+    }
+
+    public function mockeryAssertNotNull($variableName)
+    {
+        return new PhpParser\Node\Expr\MethodCall(
+            $this->variable('this'),
+            $this->name('assertNotNull'),
+            [
+                $this->variable($variableName),
+            ]
+        );
+    }
+
+    public function getParamCalling($param)
+    {
+        if ($param->isArray()) {
+            return new PhpParser\Node\Expr\Array_();
+        }
+
+        if ($param->getClass()) {
+            return $this->mockeryMock($param->getClass()->getName());
+        }
+
+        return $this->mockeryMock();
+    }
+
+    public function getMethodParamsList($objClass)
+    {
+        $methodParamsList = [];
+
+        foreach ($objClass->getParameters() as $param) {
+            $methodParamsList[] = $this->variable($this->getMethodParamVariableName($param));
+        }
+
+        return $methodParamsList;
+    }
+
+    public function addConstruct($objClass)
+    {
+        foreach ($objClass->getParameters() as $param) {
+            $this->data->addStmt(
+                new PhpParser\Node\Expr\Assign(
+                    new PhpParser\Node\Expr\Variable($this->getMethodParamVariableName($param)),
+                    $this->getParamCalling($param)
+                )
+            );
+        }
+
+        $stmt1 = new PhpParser\Node\Expr\Assign(
+            new Variable('this->target'),
+            new PhpParser\Node\Expr\New_(
+                new PhpParser\Node\Name($objClass->getTargetClassName()),
+                $this->getMethodParamsList()
+            )
+        );
+        $this->data->addStmt($stmt1);
+
+        return $this;
     }
 }
--- src/NullDev/Nemesis/Tests/Unit/CodeBuilder/NikicPhpParser/BuilderTest.php
+++ src/NullDev/Nemesis/Tests/Unit/CodeBuilder/NikicPhpParser/BuilderTest.php
@@ -3,6 +3,7 @@ namespace NullDev\Nemesis\Tests\Unit\CodeBuilder\NikicPhpParser;
 
 use NullDev\Nemesis\CodeBuilder\NikicPhpParser\Builder;
 use Mockery as m;
+use PhpParser;
 
 /**
  *
@@ -13,12 +14,77 @@ class BuilderTest extends \PHPUnit_Framework_TestCase
      * @var Builder
      */
     protected $target;
+    protected $printer;
 
     /**
      */
     protected function setUp()
     {
-        $this->target = new Builder();
+        $this->target  = new Builder();
+        $this->printer = new PhpParser\PrettyPrinter\Standard();
+    }
+
+    /**
+     *
+     */
+    public function testEmptyMethod()
+    {
+        $result = $this->target->createMethod('emptyMethod');
+
+        $expected = file_get_contents(__DIR__.'/BuilderTest/EmptyMethod.resultRender');
+
+        $this->assertEquals(
+            $expected,
+            $this->printer->prettyPrint([$result->getNode()])
+        );
+    }
+
+    /**
+     *
+     */
+    public function testSetUp()
+    {
+        $mockClass = m::mock();
+        $mockClass->shouldReceive('getTargetClassName')->once()->andReturn('stdClass');
+        $mockClass->shouldReceive('getParameters')->once()->andReturn([]);
+
+        $result = $this->target->createMethod('setUp')->addConstruct($mockClass);
+
+        $expected = file_get_contents(__DIR__.'/BuilderTest/SetUp.resultRender');
+
+        $this->assertEquals(
+            $expected,
+            $this->printer->prettyPrint([$result->getNode()])
+        );
+    }
+
+    /**
+     *
+     */
+    public function testSetUpWithConstructorParams()
+    {
+        $mockClass  = m::mock();
+        $mockParam1 = m::mock();
+        $mockParam2 = m::mock();
+
+        $mockClass->shouldReceive('getTargetClassName')->once()->andReturn('stdClass');
+        $mockClass->shouldReceive('getParameters')->once()->andReturn([$mockParam1, $mockParam2]);
+
+        $mockParam1->shouldReceive('getName')->once()->andReturn('param1');
+        $mockParam1->shouldReceive('isArray')->once()->andReturn(false);
+        $mockParam1->shouldReceive('getClass')->once()->andReturn(false);
+        $mockParam2->shouldReceive('getName')->once()->andReturn('param2');
+        $mockParam2->shouldReceive('isArray')->once()->andReturn(false);
+        $mockParam2->shouldReceive('getClass')->once()->andReturn(false);
+
+        $result = $this->target->createMethod('setUp')->addConstruct($mockClass);
+
+        $expected = file_get_contents(__DIR__.'/BuilderTest/SetUpWithConstructorParams.resultRender');
+
+        $this->assertEquals(
+            $expected,
+            $this->printer->prettyPrint([$result->getNode()])
+        );
     }
 
     /**
